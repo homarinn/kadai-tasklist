e0c1329da84d9928dae73bae6b5f405e05290be9
homarinn <homarinn1201@gmail.com>
2019-02-07 14:51:34 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,11 @@
 class TasksController < ApplicationController
-  
+  before_action :require_logged_in
   before_action :self_find, only: [:show,:edit,:update,:destroy]
   
   def index
-    @tasks=Task.all
+    if logged_in?
+      @tasks = current_user.tasks.order("created_at DESC")
+    end
   end
   
   def show
@@ -14,9 +16,9 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task=Task.new(task_params)
+    @task=current_user.tasks.build(task_params)
     if @task.save
-      redirect_to tasks_url
+      redirect_to root_url
     else
       render :new
     end
@@ -27,21 +29,30 @@ class TasksController < ApplicationController
   
   def update
     if @task.update(task_params)
-      redirect_to tasks_url
+      redirect_to root_url
     else
       render :edit
     end
   end
   
   def destroy
-    @task.destroy
-    redirect_to tasks_url
+    if @task
+      @task.destroy
+      redirect_to root_url
+    else
+      redirect_to root_url
+    end
   end
 
   private
   
   def self_find
-    @task=Task.find(params[:id])
+    @task=current_user.tasks.find_by(id: params[:id])
+    
+    # @task = current_user.tasks.find_by(id: params[:id])
+    # if (@task == nil)
+    #   redirect_to root_url
+    # end
   end
   
   def task_params
